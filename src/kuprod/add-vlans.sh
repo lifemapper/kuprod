c169b9e394a1fa36fc3ecc47096269650d37abc1
Aimee Stewart <zzeppozz@gmail.com>
2016-07-26 15:05:37 -0500

    fixed script to write commands to file

--- src/kuprod/add-vlans.sh
+++ src/kuprod/add-vlans.sh
@@ -1,49 +1,68 @@
 #!/bin/bash
 
-contains() {
-    local n=$#
-    local value=${!n}
-    for ((i=1;i < $#;i++)) {
-        if [ "${!i}" == "${value}" ]; then
-            echo "y"
+### time
+time_stamp () {
+    echo $1 `/bin/date` >> $LOG
+}
+
+### create logfile
+set_log() {
+    LOG=/tmp/`/bin/basename $0`.log
+    rm $LOG
+    touch $LOG
+}
+
+### Test whether array contains a value
+### args: container vlan# existingVlanNumbersArray
+element_in () {
+    local e
+    for e in "${@:2}"; do
+        if [ "$e" == $1 ]; then
             return 0
         fi
-    }
-    echo "n"
+    done
     return 1
 }
 
 ### Add missing vlans to vm-container
+### args: container vlan#
 add_vlans () {
-    rocks add host interface $1 vlan$2
-    rocks set host interface name $1 vlan$2 vm-container-0-0
-    rocks set host interface subnet $1 vlan$2 private
-    rocks set host interface vlan $1 vlan$2 $2
+    echo "rocks add host interface" $1 "vlan"$2 >> $LOG
+    echo "rocks set host interface name" $1 "vlan"$2 $1 >> $LOG
+    echo "rocks set host interface subnet" $1 "vlan"$2 "private" >> $LOG
+    echo "rocks set host interface vlan" $1 "vlan"$2 $2 >> $LOG
+    echo >> $LOG
 }
 
 ### Add missing vlans to vm-container ($1)
+### args: container
 test_vlans () {
     VLANS=(1 2 3 4 5 6 7)
-    echo one: $1
-    echo ${VLANS[@]}
-    EXISTING=`rocks list host interface $NAME | grep -v VLAN | awk '{print $8}'`
+    EXISTING=(`rocks list host interface $NAME | awk '{print $8}' | grep -E "^[0-9]"`)
+    echo >> $LOG
+    echo "# Container" $1 "has vlans" ${EXISTING[@]} >> $LOG
     for vlan in ${VLANS[@]}; do
-        DOIT=$(contains "${EXISTING[@]}" $vlan)
-        echo vlan: $vlan container: $1 DOIT: $DOIT
-        if [ $(contains "${EXISTING[@]}" $vlan) == "n" ]; then
-            echo Adding vlan $vlan to $1
+        if element_in $vlan "${EXISTING[@]}"; then 
+            echo 'Skipping existing vlan ' $vlan ' for container ' $1
+        else
+            echo 'Adding vlan ' $vlan ' to container ' $1
+            add_vlan $1 $vlan
         fi
-    done
+    done 
 }
 
 ### MAIN
-VMCS=`rocks list host membership vm-container | grep -v HOST | awk '{print $1}'`
-for vmc in $VMCS; do
+set_log
+time_stamp "# Start"
+
+VMCONTAINERS=`rocks list host membership vm-container | grep -v HOST | awk '{print $1}'`
+for vmc in $VMCONTAINERS; do
     # get the part before the colon 
     NAME=${vmc%:*}
-    echo name: $NAME
     test_vlans $NAME
 done
+
+time_stamp "# End"
 
 
 # /opt/rocks/bin/rocks add host interface vm-container-0-0 vlan2
