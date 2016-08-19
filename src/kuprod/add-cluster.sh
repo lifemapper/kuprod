7659c274f63c5df4799b64d83757bba2937f0e54
root <root@badenov.nhm.ku.edu>
2016-08-19 16:53:38 -0500

    fixed quoting

--- src/kuprod/add-cluster.sh
+++ src/kuprod/add-cluster.sh
@@ -5,7 +5,7 @@ VLAN=
 NODE_CONTAINER=
 
 # Defaults
-TEST=0
+TEST=1
 NUM_NODES=2
 NUM_NODE_CPUS=1
 FE_MEM_MB=16384
@@ -108,18 +108,23 @@ setParams () {
         fi
 
         TimeStamp "# Start"
-        TimeStamp "  Script called with: bash add-cluster.sh --ip=$IP \ "
-        TimeStamp "                                          --vlan=$VLAN \ "
-        TimeStamp "                                          --container_hosts=$NODE_CONTAINER \ "
-        TimeStamp "                                          --fe_size_gb=$FE_DISK_GB --fe_mem_mb=$FE_MEM_MB \ "
-        TimeStamp "                                          --node_count=$NUM_NODES --node_cpu_count=$NUM_NODE_CPUS \ "
-        TimeStamp "                                          --node_size_gb=$NODE_DISK_GB --node_mem_mb=$NODE_MEM_MB "
-
+        echo "  Script called with: bash add-cluster.sh \ " >> LOG 
+        echo "                           --ip=$IP \ " >> $LOG
+        echo "                           --vlan=$VLAN \ " >> $LOG
+        echo "                           --container_hosts=$NODE_CONTAINER \ " >> $LOG
+        echo "                           --fe_size_gb=$FE_DISK_GB --fe_mem_mb=$FE_MEM_MB \ " >> $LOG
+        echo "                           --node_count=$NUM_NODES --node_cpu_count=$NUM_NODE_CPUS \ " >> $LOG
+        echo "                           --node_size_gb=$NODE_DISK_GB --node_mem_mb=$NODE_MEM_MB " >> $LOG
     else
-        echo "Usage:  bash add-cluster.sh --ip=<ip address> --vlan=<vlan #> --container_hosts=<compute nodes host> \ "
-        echo "                            --fe_size_gb=<frontend size in gb> --fe_mem_mb=<frontend RAM in mb> \ "
-        echo "                            --node_count=<number of compute nodes> --node_cpu_count=<cores per node> \ "
-        echo "                            --node_size_gb=<node size in gb> --node_mem_mb=<node RAM in mb> "
+        echo "Usage:  bash add-cluster.sh --ip=<ip address> \ "
+        echo "                            --vlan=<vlan #> \ "
+        echo "                            --container_hosts=<compute nodes host> \ "
+        echo "                            --fe_size_gb=<frontend size in gb> \ "
+        echo "                            --fe_mem_mb=<frontend RAM in mb> \ "
+        echo "                            --node_count=<number of compute nodes> \ " 
+        echo "                            --node_cpu_count=<cores per node> \ "
+        echo "                            --node_size_gb=<node size in gb> \ "
+        echo "                            --node_mem_mb=<node RAM in mb> "
         echo "        Possible node container hosts are: $vchosts"
         exit 1
     fi
@@ -158,7 +163,7 @@ setDisksMem () {
     cnodes=`rocks list cluster $FE_NAME | tail -n +3 |  awk '{print $2}'`
     for h in $cnodes;
     do  
-        cmd="rocks set host vm $h disk="file:/state/partition1/kvm/disks/$h.vda,vda,virtio"
+        cmd="rocks set host vm $h disk="file:/state/partition1/kvm/disks/$h.vda,vda,virtio""
         TimeStamp "Setting node disk with command: $cmd"
         $COM $cmd
     done
@@ -172,16 +177,20 @@ setDisksMem () {
        TimeStamp "ZFS tank/vms/$FE_NAME exists"
     fi
 
-    cmd="rocks set host vm $FE_NAME disk="file:/tank/vms/kvm/disks/$FE_NAME.vda,vda,virtio" disksize=$FE_DISK_GB
+    cmd="rocks set host vm $FE_NAME disk="file:/tank/vms/kvm/disks/$FE_NAME.vda,vda,virtio" disksize=$FE_DISK_GB"
     TimeStamp "Setting FE disk with command $cmd"
     $COM $cmd
 
-    cmd="rocks set host vm $FE_NAME mem=$FE_MEM_B"
+    cmd="rocks set host vm $FE_NAME mem=$FE_MEM_MB"
     TimeStamp "Setting FE disk with command $cmd"
     $COM $cmd
+
 }
 
 setParams
 addCluster
 setDisksMem
-                                                                                                                                                    177,11        Bot
+
+
+##  bash add-cluster.sh --ip=129.237.201.194 --container_hosts=badenov --fe_size_gb=1000 --fe_mem_mb=16384 --node_count=2 --node-cpu=4 --node_size_gb=100 --node_mem_mb=8194 --test=1
+## 
