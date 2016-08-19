9d69f3ee418072b234da97d0841a1e01c9e9a686
Aimee Stewart <zzeppozz@gmail.com>
2016-08-19 11:59:14 -0500

    simplify

--- src/kuprod/write-vlan-commands.sh
+++ src/kuprod/write-vlan-commands.sh
@@ -7,8 +7,8 @@ time_stamp () {
 
 ### create logfile
 set_log() {
-    SCRIPT=/tmp/`/bin/basename $0`.sh
-    rm $SCRIPT
+    SCRIPT=/tmp/add-vlans.sh
+    rm -f $SCRIPT
     touch $SCRIPT
 }
 
@@ -31,30 +31,30 @@ write_vlan () {
     echo "rocks set host interface name" $1 "vlan"$2 $1 >> $SCRIPT
     echo "rocks set host interface subnet" $1 "vlan"$2 "private" >> $SCRIPT
     echo "rocks set host interface vlan" $1 "vlan"$2 $2 >> $SCRIPT
-    echo >> $SCRIPT
 }
 
-### Add missing vlans to vm-container ($1)
+### Add missing vlans to fe or vm-container ($1)
 ### args: container
 add_vlan_commands () {
     VLANS=(1 2 3 4 5 6 7)
-    EXISTING=(`rocks list host interface $NAME | awk '{print $8}' | grep -E "^[0-9]"`)
-    echo >> $SCRIPT
+    EXISTING=(`rocks list host interface $1 | awk '{print $8}' | grep -E "^[0-9]"`)
     echo "# Container" $1 "has vlans" ${EXISTING[@]} >> $SCRIPT
     for vlan in ${VLANS[@]}; do
-        if element_in $vlan "${EXISTING[@]}"; then 
-            echo 'Skipping existing vlan ' $vlan ' for container ' $1
-        else
-            echo 'Adding vlan ' $vlan ' to container ' $1
+        if ! element_in $vlan "${EXISTING[@]}" ; then
+            echo 'Add vlan ' $vlan ' to container ' $1
             write_vlan $1 $vlan
         fi
-    done 
+    done
+    echo >> $SCRIPT
 }
 
 ### MAIN
 set_log
 time_stamp "# Start"
 
+tmp=`rocks list host | grep Frontend | grep -v vm | awk '{print $1}'`
+FE=${tmp%:*}
+add_vlan_commands $FE
 VMCONTAINERS=`rocks list host membership vm-container | grep -v HOST | awk '{print $1}'`
 for vmc in $VMCONTAINERS; do
     # get the part before the colon 
@@ -64,3 +64,4 @@ done
 echo "rocks sync config" >> $SCRIPT
 
 time_stamp "# End"
+
